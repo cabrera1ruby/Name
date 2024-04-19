9cb552fbb6654b3bc70cc527a37a6eb8781d5450
cabrera1ruby <165818020+cabrera1ruby@users.noreply.github.com>
2024-04-19 08:50:47 -0700

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,18 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) { 
+    for(int i = 0; i < 3; i++){
+      System.out.println("Ruby!");
+    }
+
+    
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
